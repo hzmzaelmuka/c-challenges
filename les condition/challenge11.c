6742082cbaebca30c5f83825eb4550d6e277a852
hamza <you@example.com>
2025-09-23 15:04:39 +0200

    pair

---
+++ les condition/challenge11.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+    int a;
+  printf("entre un nombre  :");
+  scanf("%d",&a);
+  
+  if(a%2!=0)
+  printf("le nombre et impair");
+  else 
+  printf("les nombre et pair");
+  return 0;
+}
