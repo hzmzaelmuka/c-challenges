a0f6fb5f15b781922d70da3dcfe5f7dc6c23ec0a
hamza <almouka18@gmail.com>
2025-09-23 16:28:52 +0200

    somme

---
+++ les condition/challenge.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int A ,B , S;
+printf("entre doux nombre ");
+scanf("%d%d",&A,&B);
+S=A+B;
+if(A==B)
+printf("alour le tripal de la somme et %d",S*S*S);
+else
+printf("la somme et:%d:",S);
+return 0 ;
+
+}
